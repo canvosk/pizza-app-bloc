f4790d67c74a673315fd363fc3aa453370af6844
Can BELLİBAŞLILAR <bellibasllarcn@gmail.com>
2023-10-22 14:48:58 +0300

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_app_bloc/models/pizza_model.dart';
+import 'package:pizza_app_bloc/pizza_bloc/pizza_bloc.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,15 +13,119 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
+
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => PizzaBloc()..add(LoadPizzaCounter()),
+          ),
+        ],
+        child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "The Pizza Bloc",
+          home: HomeScreen(),
+        ));
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("The Pizza Bloc"),
+        centerTitle: true,
+        backgroundColor: Colors.orange[800],
+      ),
+      body: Center(
+        child: BlocBuilder<PizzaBloc, PizzaState>(
+          builder: (context, state) {
+            if (state is PizzaInitial) {
+              return const CircularProgressIndicator(
+                color: Colors.orange,
+              );
+            }
+            if (state is PizzaLoaded) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '${state.pizzas.length}',
+                    style: const TextStyle(
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height / 1.5,
+                    width: MediaQuery.of(context).size.width,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      clipBehavior: Clip.none,
+                      children: [
+                        for (int index = 0;
+                            index < state.pizzas.length;
+                            index++)
+                          Positioned(
+                            left: Random().nextInt(250).toDouble(),
+                            right: Random().nextInt(400).toDouble(),
+                            child: SizedBox(
+                              height: 150,
+                              width: 150,
+                              child: state.pizzas[index].image,
+                            ),
+                          )
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return const Text("Something went wrong!");
+            }
+          },
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              context.read<PizzaBloc>().add(AddPizza(Pizza.pizzas[0]));
+            },
+            backgroundColor: Colors.orange[800],
+            child: const Icon(Icons.local_pizza_outlined),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              context.read<PizzaBloc>().add(RemovePizza(Pizza.pizzas[0]));
+            },
+            backgroundColor: Colors.orange[800],
+            child: const Icon(Icons.remove),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              context.read<PizzaBloc>().add(AddPizza(Pizza.pizzas[1]));
+            },
+            backgroundColor: Colors.orange[500],
+            child: const Icon(Icons.local_pizza),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              context.read<PizzaBloc>().add(RemovePizza(Pizza.pizzas[1]));
+            },
+            backgroundColor: Colors.orange[500],
+            child: const Icon(Icons.remove),
+          ),
+        ],
       ),
     );
   }
